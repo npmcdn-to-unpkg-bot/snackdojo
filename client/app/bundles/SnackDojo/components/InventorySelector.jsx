16a33418777731c711474283b5df723ce85537ee
Benjamin Shyong <ben.shyong@gmail.com>
2016-02-04 00:35:59 -0800

    inventory styling

--- client/app/bundles/SnackDojo/components/InventorySelector.jsx
+++ client/app/bundles/SnackDojo/components/InventorySelector.jsx
@@ -15,18 +15,18 @@ export default class InventorySelector extends React.Component {
     const { id, name, picture } = item
 
     return (
-      <div key={id} className="p1 center">
-        <div className="flex flex-center" style={{ width: '150px', height: '150px' }}>
-          <img src={picture || 'http://placehold.it/150x150'} style={{ maxWidth: '150px', maxHeight: '150px' }} className="mx-auto" />
+      <div key={id} className="p1 center flex-auto">
+        <div className="flex flex-center mx-auto" style={{ width: '150px', height: '150px' }}>
+          <img src={picture || 'http://placehold.it/150x150'} style={{ maxWidth: '150px', maxHeight: '150px', height: 'auto', width: 'auto' }} className="mx-auto" />
         </div>
-        <div>{name}</div>
+        <div className="py1">{name}</div>
       </div>
     )
   }
 
   renderItemGroup = (items, groupKey) => {
     return (
-      <div key={groupKey} className="py2">
+      <div key={groupKey} className="py2 border-bottom">
         <h3>{groupKey}</h3>
         <div className="flex flex-center flex-wrap">
           {items.map(this.renderItem)}
@@ -40,10 +40,15 @@ export default class InventorySelector extends React.Component {
     const groupedItems = _.groupBy(items, 'category')
 
     return (
-      <div>
-        {
-          _.map(groupedItems, this.renderItemGroup)
-        }
+      <div className="clearfix">
+        <div className="sm-col-9 sm-col">
+          {
+            _.map(groupedItems, this.renderItemGroup)
+          }
+        </div>
+        <div className="sm-col-3 sm-col center">
+          <h3>Your box</h3>
+        </div>
       </div>
     );
   }
