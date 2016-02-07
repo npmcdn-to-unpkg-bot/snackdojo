22ec70da132d7cb89971c9e0201827d854532ed9
Benjamin Shyong <ben.shyong@gmail.com>
2016-02-06 23:11:15 -0800

    add price to item serializer'

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,7 +1,11 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :category, :picture
+  attributes :id, :name, :description, :category, :picture, :price
 
   def category
     object.category.try(:name)
+  end
+
+  def price
+    object.price || 0
   end
 end
