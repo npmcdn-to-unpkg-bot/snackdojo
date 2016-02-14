d7ac33d7cfb151a3a2aa9af0667dff1d6d2313c1
Benjamin Shyong <ben.shyong@gmail.com>
2016-02-13 23:59:07 -0800

    use locale file for persona copy

--- app/serializers/persona_serializer.rb
+++ app/serializers/persona_serializer.rb
@@ -1,3 +1,7 @@
 class PersonaSerializer < ActiveModel::Serializer
   attributes :id, :name, :description
+
+  def description
+    I18n.t("personas.#{name.try(:downcase)}", default: object.description)
+  end
 end
